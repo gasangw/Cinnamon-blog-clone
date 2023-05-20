82f0add4258d5f4a9ec86e021d653b85a5eeba45
gasangw <thomasgasangwa@gmail.com>
2023-05-20 11:45:24 +0200

    Add images to use and layout for three articles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
+    extend: {
+      colors: {
+       "background": "#1C1C1C",
+      },
+    },
   },
   plugins: [],
 }
